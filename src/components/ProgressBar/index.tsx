99c501c7a5510fa3d956244a0004fc926d07f88b
龍品瑞 <nicks96432@gmail.com>
2022-06-06 15:01:12 +0800

    use yarn v1

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -4,7 +4,10 @@ import "./ProgressBar.css";
 
 const ProgressBar = ({ completed }: { completed: number }) => (
   <div className="progress-bar">
-    <div className="progress-bar__fill" style={{ width: `${completed}%` }}></div>
+    <div
+      className="progress-bar__fill"
+      style={{ width: `${completed}%` }}
+    ></div>
   </div>
 );
 
